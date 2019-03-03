88efc480356db69426cccbc563424e7823100c6c
Wizzac <cuksebastian@gmail.com>
2019-03-02 22:42:48 -0300

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Navbar.css';
+//import stl from 'styled-components';
+
+
+
+
+
+export default class Navbar extends React.Component {
+  
+    render() {
+      return (
+        <div className="ct" id="t1">
+        <div className="ct" id="t2">
+          <div className="ct" id="t3">
+            <div className="ct" id="t4">
+              <section>   
+                JUSTO ACAAAAA
+              </section>
+            </div>
+          </div>
+        </div>
+       </div>
+      )
+    }
+
+    
+  }
+  
